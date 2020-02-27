532574e2687a46fda111298111dd1e2bb5766bde
Thol Chidambaram <tholkappia@gmail.com>
2020-02-27 00:17:48 -0800

    added queries and mutations for classes and students

--- bin/app.py
+++ bin/app.py
@@ -1,16 +1,35 @@
 import copy
-from ariadne import QueryType, graphql_sync, make_executable_schema
+from ariadne import QueryType, MutationType, graphql_sync, make_executable_schema
 from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, request, jsonify
 
 type_defs = """
+
+    type Student {
+        name: String
+    }
+
+    type Class {
+        name: String
+    }
+
     type Query {
         hello: String!
+        query_student(id: Int!): Student
+        query_class(id : Int!): Class
+    }
+
+    type Mutation {
+        create_student(name: String!): Student
+        create_class(name: String!): Class
     }
 """
 
 query = QueryType()
+mutation = MutationType()
 
+student = []
+class_dict = []
 
 @query.field("hello")
 def resolve_hello(_, info):
@@ -18,8 +37,29 @@ def resolve_hello(_, info):
     user_agent = request.headers.get("User-Agent", "Guest")
     return "Hello, %s!" % user_agent
 
+@query.field("query_student")
+def resolve_query_student(*_, id):
+    if len(student) > id:
+        return student[id]
+    return "Not found"
 
-schema = make_executable_schema(type_defs, query)
+@mutation.field("create_student")
+def resolve_create_student(*_, name):
+    student.append({"name":name})
+    return student[-1]
+
+@query.field("query_class")
+def resolve_query_class(*_, id):
+    if len(class_dict) > id:
+        return class_dict[id]
+    return "Not found"
+
+@mutation.field("create_class")
+def resolve_create_class(*_, name):
+    class_dict.append({"name":name})
+    return class_dict[-1]
+
+schema = make_executable_schema(type_defs, query, mutation)
 
 app = Flask(__name__)
 
@@ -37,6 +77,7 @@ def graphql_playgroud():
 def graphql_server():
     # GraphQL queries are always sent as POST
     data = request.get_json()
+
 
     # Note: Passing the request to the context is optional.
     # In Flask, the current request is always accessible as flask.request
